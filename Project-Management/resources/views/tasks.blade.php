206065bb9e844a4a850ff524cbde5713d2fe0d10
amnasohaib <amnasohaib2003@gmail.com>
2023-12-19 12:49:46 +0500

    Update tasks.blade.php

--- Project-Management/resources/views/tasks.blade.php
+++ Project-Management/resources/views/tasks.blade.php
@@ -8,8 +8,10 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" />
-    <link rel="stylesheet" href="./css/calendar.css" />
-    <link rel="stylesheet" href="./css/Sidebar.css" />
+    <!-- <link rel="stylesheet" href="./css/calendar.css" />
+    <link rel="stylesheet" href="./css/Sidebar.css" /> -->
+
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/Sidebar.css') }}">
 
 </head>
 
